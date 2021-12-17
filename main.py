a89e6049590885004035bd8b3af7f3a258dee7a5
Andrey Ivlev <warcraft1921@gmail.com>
2021-12-17 16:29:01 +0300

    I just wanna fucking die. Cant find a single mistake

--- main.py
+++ main.py
@@ -6,22 +6,6 @@ import matplotlib.pyplot as plt
 # alpha1 * y(l) + beta1 * y'(l) = gamma1
 
 
-def p(x):
-    return np.cosh(x)
-
-
-def q(x):
-    return np.sinh(x)
-
-
-def f(x):
-    return np.cosh(x) + x * np.sinh(x)
-
-
-def solution(x):
-    return np.exp(-np.sinh(x)) + x
-
-
 def least_squares(x, y):
     n = len(x)
 
@@ -38,57 +22,51 @@ def least_squares(x, y):
 
 
 def solve1(p, q, f, alpha, beta, gamma, x0, xend, h):
-    xrange = np.arange(x0, xend, h)
+    xrange = np.linspace(x0, xend, int(((xend - x0) // h) + 1))
 
     a = np.zeros(len(xrange))
-    for i, x in zip(range(1, len(xrange) - 1), xrange[1:-1]):
-        a[i] = 1 - ((p(x) * h) / 2)
-    a[-1] = - beta[1] * h
-
     b = np.zeros(len(xrange))
-    b[0] = alpha[0] * (h**2) - beta[0] * h
-    for i, x in zip(range(1, len(xrange) - 1), xrange[1:-1]):
-        b[i] = -2 + q(x) * (h**2)
-    b[-1] = alpha[1] * (h**2) + beta[1] * h
-
     c = np.zeros(len(xrange))
-    c[0] = beta[0] * h
-    for i, x in zip(range(1, len(xrange) - 1), xrange[1:-1]):
-        c[i] = 1 + ((p(x) * h) / 2)
+    p_array = np.vectorize(p)(xrange)
+    q_array = np.vectorize(q)(xrange)
+    f_array = np.vectorize(f)(xrange)
 
-    f_array = np.zeros(len(xrange))
-    f_array[0] = gamma[0] * (h**2)
+    b[0] = alpha[0] * (h ** 2) - beta[0] * h
+    c[0] = beta[0] * h
+    f_array[0] = gamma[0] * (h ** 2)
+
+    a[1:-1] = 1 - ((p_array[1:-1] * h) / 2)
+    b[1:-1] = -2 + q_array[1:-1] * (h ** 2)
+    c[1:-1] = 1 + ((p_array[1:-1] * h) / 2)
+    f_array[1:-1] = f_array[1:-1] * (h ** 2)
+
+    a[-1] = - beta[1] * h
+    b[-1] = alpha[1] * (h**2) + beta[1] * h
     f_array[-1] = gamma[1] * (h**2)
-    for i, x in zip(range(1, len(xrange) - 1), xrange[1:-1]):
-        f_array[i] = f(x) * (h**2)
 
     return solve3diagonal(a, b, c, f_array)
 
 
 def solve2(p, q, f, alpha, beta, gamma, x0, xend, h):
-    xrange = np.arange(x0, xend, h)
+    xrange = np.linspace(x0, xend,  int(((xend - x0) // h) + 1))
 
     a = np.zeros(len(xrange))
-    for i, x in zip(range(1, len(xrange) - 1), xrange[1:-1]):
-        a[i] = 1 - ((p(x) * h) / 2)
-    a[-1] = 2
-
     b = np.zeros(len(xrange))
-    b[0] = q(x0) * h**2 - 2 + (2 * h * alpha[0]) / beta[0] - (alpha[0] * p(x0) * h**2) / beta[0]
-    for i, x in zip(range(1, len(xrange) - 1), xrange[1:-1]):
-        b[i] = -2 + q(x) * (h**2)
-    b[-1] = q(xend) * h**2 - 2 - (2 * h * alpha[1]) / beta[1] - (alpha[1] * p(xend) * h**2) / beta[1]
-
     c = np.zeros(len(xrange))
-    c[0] = 2
-    for i, x in zip(range(1, len(xrange) - 1), xrange[1:-1]):
-        c[i] = 1 + ((p(x) * h) / 2)
-
     f_array = np.zeros(len(xrange))
-    f_array[0] = f(x0) * h**2 + (2 * h * gamma[0]) / beta[0] - (p(x0) * gamma[0] * h**2) / beta[0]
+
+    b[0] = q(x0) * h ** 2 - 2 + (2 * h * alpha[0]) / beta[0] - (alpha[0] * p(x0) * h ** 2) / beta[0]
+    c[0] = 2
+    f_array[0] = f(x0) * h ** 2 + (2 * h * gamma[0]) / beta[0] - (p(x0) * gamma[0] * h ** 2) / beta[0]
+
+    a[1:-1] = 1 - ((p(xrange[1:-1]) * h) / 2)
+    b[1:-1] = -2 + q(xrange[1:-1]) * (h ** 2)
+    c[1:-1] = 1 + ((p(xrange[1:-1]) * h) / 2)
+    f_array[1:-1] = f(xrange[1:-1]) * (h ** 2)
+
+    a[-1] = 2
+    b[-1] = q(xend) * h**2 - 2 - (2 * h * alpha[1]) / beta[1] - (alpha[1] * p(xend) * h**2) / beta[1]
     f_array[-1] = f(xend) * h**2 - (2 * h * gamma[1]) / beta[1] - (p(xend) * gamma[1] * h**2) / beta[1]
-    for i, x in zip(range(1, len(xrange) - 1), xrange[1:-1]):
-        f_array[i] = f(x) * (h**2)
 
     return solve3diagonal(a, b, c, f_array)
 
@@ -128,23 +106,27 @@ def solve3diagonal_thompson(a, b, c, f):
         y[i] = F[i] - C[i] * y[i + 1]
     return y
 
+
 def main1():
     x0 = 0
     xend = 1
 
-    alpha = np.array([[0], [6]])
-    beta = np.array([[1], [1]])
-    gamma = np.array([[0], [8.3761043995962756169530107919075058250981216852045948197363873469]])
+    alpha = np.array([0, 6])
+    beta = np.array([1, 1])
+    gamma = np.array([0, 8.3761043995962756169530107919075058250981216852045948197363873469])
 
     # u'(0) = 0
     # 6u(1) + u'(1) = 8.3761
 
-    sol = solution
+    p = np.cosh
+    q = np.sinh
+    f = lambda x: np.cosh(x) + x * np.sinh(x)
+    solution = lambda x: np.exp(-np.sinh(x)) + x
 
     h = 0.01
     xrange = np.arange(x0, xend, h)
 
-    y = solve1(p, q, f, alpha, beta, gamma, x0, xend, h)
+    # y = solve1(p, q, f, alpha, beta, gamma, x0, xend, h)
     y = solve2(p, q, f, alpha, beta, gamma, x0, xend, h)
 
     number_of_methods = 1
@@ -155,7 +137,7 @@ def main1():
     plt.ylabel("u(x)")
     plt.grid()
     plt.plot(xrange, y, color='k', label='Численное значение')
-    plt.plot(xrange, sol(xrange), ls='--', color='k', label='Аналитическое значение')
+    plt.plot(xrange, solution(xrange), ls='--', color='k', label='Аналитическое значение')
 
     plt.show()
 
@@ -164,21 +146,26 @@ def main2():
     x0 = 0
     xend = 1
 
-    alpha = np.array([[0], [6]])
-    beta = np.array([[1], [1]])
-    gamma = np.array([[0], [8.3761043995962756169530107919075058250981216852045948197363873469]])
+    alpha = np.array([0, 6])
+    beta = np.array([1, 1])
+    gamma = np.array([0, 8.3761043995962756169530107919075058250981216852045948197363873469])
+
+    p = np.cosh
+    q = np.sinh
+    f = lambda x: np.cosh(x) + x * np.sinh(x)
+    solution = lambda x: np.exp(-np.sinh(x)) + x
 
     hmin = 0.0001
-    hmax = 0.1
-    hstep = 0.0001
-    hrange = np.arange(hmin, hmax, hstep)
+    hmax = 0.005
+    number_of_steps = 100
+    hrange = np.linspace(hmin, hmax, number_of_steps)
 
     error = dict()
     error[solve1] = np.zeros(len(hrange))
     error[solve2] = np.zeros(len(hrange))
 
     for i, h in zip(range(len(hrange)), hrange):
-        sol = solution(np.arange(x0, xend, h))
+        sol = np.vectorize(solution)(np.linspace(x0, xend, int(((xend - x0) // h) + 1)))
         for key in error:
             error[key][i] = np.max(np.abs(key(p, q, f, alpha, beta, gamma, x0, xend, h) - sol))
 
@@ -198,6 +185,7 @@ def main2():
     for key in error:
         coeffs = least_squares(hrange, error[key])
         print(key.__name__, ": ", coeffs[0], " + ", coeffs[1], "x", sep="")
+
     plt.show()
 
 
